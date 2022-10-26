99f5d7ef6204c22c324f36188068042c64e4f7d6
GustavoVictorQuaresma <gustavic99@gmail.com>
2022-10-26 20:14:40 -0300

    correcao_26_10_22

---
+++ atividade_corrigida_26_10_22/atividade1.py
@@ -0,0 +1,23 @@
+# programa para receber dois números inteiros e realizar todas operações matemáticas
+# incluindo resto da divisão e potência
+
+print("Programa para calcular operações matemáticas")
+num1 = float(input("Digite o primeiro número inteiro: "))
+num2 = float(input("Digite o segundo número inteiro: "))
+resultado = num1 + num2
+print("A soma do número 1 + número 2 é: ", resultado)
+
+resultado2 = num1 - num2
+print("A subtração do número 1 - número 2 é: ", resultado2)
+
+resultado3 = num1 * num2
+print ("A multiplicação do número 1 * número 2 é: ", resultado3)
+
+resultado4 = num1 / num2
+print("A divisão do número 1 / número 2 é: ", resultado4)
+
+resultado5 = num1 % num2
+print("O resto da divisão do número 1 / número 2 é ", resultado5)
+
+resultado6 = num1 ** num2
+print("A potência do número 1 pelo número 2 é: ", resultado6)
