f7f7cf0352a1de43c65b08642b326fc7fb378666
GustavoVictorQuaresma <gustavic99@gmail.com>
2022-10-25 21:55:56 -0300

    add.exercicio2.py

---
+++ exercicio2.py
@@ -0,0 +1,13 @@
+# Crie um programa para receber valores em variáveis de string e inteiro e mostre os valores ao final.
+
+# inteiro
+# real
+
+a = input("Digite um número inteiro")
+int(a)
+print(a)
+
+b = input("Digite um número real")
+float(b)
+print(b)
+
