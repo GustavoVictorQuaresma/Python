d14b84a86933062b740659267055226488d8b3dc
GustavoVictorQuaresma <gustavic99@gmail.com>
2022-10-25 21:22:37 -0300

    add.exercicio1.py

---
+++ exercicio1.py
@@ -0,0 +1,11 @@
+# Crie um programa que contenha as variáveis a, b, c, d.
+
+a = 2
+b = 2.1
+c = "Gustavo"
+d = False
+
+print("O valor da variável a é: " + str(a))
+print("O valor da variável b é: " + str(b))
+print("O valor da variável c é: " + str(c))
+print("O valor da variável d é: " + str(d))
