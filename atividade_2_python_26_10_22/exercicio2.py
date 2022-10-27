6d8d6f71cf63f38a8ac57a52c07cbd8f02ecceae
GustavoVictorQuaresma <gustavic99@gmail.com>
2022-10-27 19:46:46 -0300

    correcao_27_10_22

--- atividade_2_python_26_10_22/exercicio2.py
+++ atividade_2_python_26_10_22/exercicio2.py
@@ -3,10 +3,8 @@
 
 n1 = float(input("Digite um número real: "))
 if (n1 > 10):
-    print("O número digitado é maior que 10:", n1)
-
+    print("O número digitado é maior que 10:")
 elif (n1 == 10):
-    print("O número digitado é igual a 10:", n1)
-
-else
-    print("O número digitado é menor que 10":, n1)
+    print("O número digitado é igual a 10:")
+elif (n1 < 10)
+    print("O número digitado é menor que 10":)
--- atividade_corrigida_26_10_22/exercicio1.py
+++ atividade_corrigida_26_10_22/exercicio1.py
@@ -1,15 +1,2 @@
-# Crie um programa que contenha as variáveis a, b, c, d.
+# programa receba 5 produtos em variáveis constantes
 
-a = 10
-b = 10.5
-c = "Gustavo"
-d = True
-
-print("O valor da variável a é: " + str(a) + " e o tipo dela é ")
-print(type(a))
-print("O valor da variavel b é: " + str(b) + " e o tipo dela é")
-print(type(b))
-print("O Valor da varável c é: " + str(c) + " e o tipo dela é")
-print(type(c))
-print("O valor da variável d é : " + str(d) + " e o tipo dela é")
-print(type(d))
