339c5f56ed95a16078c36b45b42bf86eed679cd1
GustavoVictorQuaresma <gustavic99@gmail.com>
2022-10-26 21:56:58 -0300

    correcao_26_10_22

---
+++ atividade_26_10_22/exercicio2.py
@@ -0,0 +1,12 @@
+# Programa que receba um numero digitado pelo usuário, que será comparado se o numero digitado é
+# maior que 10, igual a 10 ou menor que 10
+
+n1 = float(input("Digite um número real: "))
+if (n1 > 10):
+    print("O número digitado é maior que 10:", n1)
+
+elif (n1 == 10):
+    print("O número digitado é igual a 10:", n1)
+
+else
+    print("O número digitado é menor que 10":, n1)
