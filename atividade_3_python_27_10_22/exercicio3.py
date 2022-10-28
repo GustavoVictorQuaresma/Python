73a3b4c091884bc552c4a3012ffa90964ca522a6
GustavoVictorQuaresma <gustavic99@gmail.com>
2022-10-27 21:56:52 -0300

    correcao_27_10_22

---
+++ atividade_3_python_27_10_22/exercicio3.py
@@ -0,0 +1,11 @@
+# Crie um programa de calculadora que realiza as operações de soma, multiplicação, divisão e subtração, será perguntado
+# ao usuário se deseja continuar com o uso da calculadora, enquanto ele digitar (“S” – Sim) o programa irá reiniciar a
+# calculadora.
+
+print("Programa Tabuada Enquanto")
+num1 = int(input("Digite o primeiro número inteiro: "))
+print(num1)
+
+num2 = int(input("Digite o segundo número inteiro: "))
+print(num2)
+
