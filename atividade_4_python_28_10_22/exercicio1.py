3674391ad9522d3a9bd93b330007cb2022837b5e
GustavoVictorQuaresma <gustavic99@gmail.com>
2022-10-28 20:01:17 -0300

    correcao_28_10_22

---
+++ atividade_4_python_28_10_22/exercicio1.py
@@ -0,0 +1,6 @@
+# Crie um programa para calcular as notas obtidas pelo aluno do ensino médio, deverá mostrar mensagem para ser digitado a
+# nota do 1º, 2º, 3º e 4º bimestre. Após deverá mostrar na tela se o aluno foi aprovado, se está em recuperação ou
+# foi reprovado sem chance de recuperação.
+# Lembrando que cada bimestre vale 25 pontos num total anual de 100 pontos.
+
+print("Programa Notas obtidas ")
