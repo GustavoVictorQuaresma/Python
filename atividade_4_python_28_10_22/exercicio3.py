4bd89ae4d16bacc42450d98f83caafc39ca6d10a
GustavoVictorQuaresma <gustavic99@gmail.com>
2022-10-28 21:54:54 -0300

    correcao_28_10_22

--- atividade_4_python_28_10_22/exercicio3.py
+++ atividade_4_python_28_10_22/exercicio3.py
@@ -3,4 +3,19 @@
 # Ao final, os índices da linha e da coluna devem ser impressos se o elemento for encontrado;
 # caso contrário, a mensagem “elemento não encontrado” deve ser mostrada na tela.
 
+print("Programa Matriz")
 
+M = [[0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4], [0, 1, 2, 3, 4]]
+for i in range(4):
+    for j in range(4):
+        M[i][j] = int(input("Digite os números inteiros para adicionar na matriz: "))
+
+num = int(input("Digite um número inteiro para procurar na matriz: "))
+
+for i in range(4):
+    for j in range(4):
+        if M[i][j] == num:
+            print("Numéro encontrado !")
+
+        else:
+            print("Número não encontrado !")
